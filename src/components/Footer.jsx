09907d422298bbebf26c89574d9e2a0a6f7d99f7
Sang-ATTN-UIT <112749745+Sang-ATTN-UIT@users.noreply.github.com>
2022-11-29 13:55:58 +0700

    Up load by hand

---
+++ src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="mt-24">
+    <p className="dark:text-gray-200 text-gray-700 text-center ">
+      Nhóm thực hiện: 06
+    </p>
+    <p className= "dark:text-gray-200 text-gray-700 text-center m-2">
+      Nguyễn Hữu Minh Sang - 20520921
+      </p>
+    <p className="dark:text-gray-200 text-gray-700 text-center m-2">
+      Nguyễn Hữu Minh Sang - 20520921
+    </p>
+    <p className="dark:text-gray-200 text-gray-700 text-center m-2">
+      Nguyễn Hữu Minh Sang - 20520921
+    </p>
+  </div>
+);
+
+export default Footer;
