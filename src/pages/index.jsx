8426fbb9221bba340fdb6a5dddde79b68a0e2a0d
Sang-ATTN-UIT <20520921@gm.uit.edu.vn>
2022-11-29 13:25:47 +0700

    fist

---
+++ src/pages/index.jsx
@@ -0,0 +1,7 @@
+export { default as Kanban } from './Kanban';
+export { default as Orders } from './Orders';
+export { default as Editor } from './Editor';
+export { default as ColorPicker } from './ColorPicker';
+export { default as Calendar } from './Calendar';
+export { default as Home } from './Home'
+
